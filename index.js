74028fc2464c786bd5a1c523f50d49baa0dfeb3d
Jordan Harband <ljharb@gmail.com>
2020-02-03 12:12:22 -0800

    Implementation

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const pacote = require('pacote');
+const semverCompare = require('semver/functions/compare-build');
+const fromEntries = require('object.fromentries');
+
+const sortAsc = ([a], [b]) => semverCompare(a, b);
+const sortDesc = ([a], [b]) => -semverCompare(a, b);
+
+module.exports = async function getPublishers(packageName, ascending = undefined) {
+	if (typeof packageName !== 'string' || packageName.length === 0) {
+		throw new TypeError('`packageName` must be a non-empty string');
+	}
+	if (arguments.length > 1 && typeof ascending !== 'boolean') {
+		throw new TypeError('`ascending` must be `true` or `false`');
+	}
+	const packument = await pacote.packument(packageName, { fullMetadata: true });
+	const entries = Object.entries(packument.versions)
+		.map(([v, { _npmUser: u }]) => [`v${v}`, { ...u, created: packument.time[v] }])
+		.sort(void ascending === 'undefined' || ascending ? sortAsc : sortDesc);
+	return fromEntries(entries);
+};
